488398a4f208085b7772edb613e58e3eee9ed9d7
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-05-05 10:29:49 -0400

    Create 063-Song.hpp

---
+++ Codecademy/C++/063-Song.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+// add the Song class here:
+class Song {
+  // Attributes
+  std::string title;
+
+  // Methods
+public:
+  void add_title(std::string new_title);
+  std::string get_title();
+  
+};
