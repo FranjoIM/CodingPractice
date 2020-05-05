4873a13c438c993420b32f4aa3242c9472e4a80e
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-05-04 21:09:19 -0400

    Create 01-Letter.cpp

---
+++ Codecademy/01-Letter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "1. Learn C++\n";
+  std::cout << "2. Learn R\n";
+  std::cout << "3. Learn Python\n";
+  std::cout << "4. Learn Perl\n";
+  std::cout << "Franjo, 04/26/2020\n";
+  
+}
