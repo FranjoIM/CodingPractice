4bd49cd2e5c33d9ab1a93251271a04df731b70fa
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-05-05 14:41:02 -0400

    Create 08-index.php

---
+++ Codecademy/PHP/08-index.php
@@ -0,0 +1,15 @@
+<?php
+  echo "I'm going on a picnic!";
+
+  $sentence = "\nI'm going on a picnic, and I'm taking apples";
+
+  echo $sentence;
+
+// Write your code below:
+  $sentence .= ", bread";
+  echo $sentence;
+
+  $sentence .= ", cheese.";
+  echo $sentence; 
+
+?>
