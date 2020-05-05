ee04d6eeebf73b2a8b258d7e39399905466a615e
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-05-05 14:23:04 -0400

    Create 04-index.php

---
+++ Codecademy/PHP/04-index.php
@@ -0,0 +1,6 @@
+<?php
+// Write your code below:
+  $name = "Franjo";
+  $biography = "\nMy name is Franjo. I'm a PhD student in the Genetics and Genomics program at the University of Florida. My passion is intersection of genetics, statistics, psychology, and anthropology.";
+  $favorite_food = "\n" . "tur" . "duck" . "en";
+?>
