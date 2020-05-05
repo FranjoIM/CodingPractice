6a86d494da39948909f43b20fc8c41540a34aa33
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-05-05 14:37:12 -0400

    Create 07-index.php

---
+++ Codecademy/PHP/07-index.php
@@ -0,0 +1,16 @@
+<?php
+  $movie = "___";
+// Add your code here:
+  $movie = "Crouching Tier, Hidden Dragon";
+  $old_favorite = $movie;
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+// Add a statement here:
+  $movie = "Thor: Ragnarok";
+  
+  echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+  echo "Though I did enjoy watching $old_favorite recently again.";
+?>
