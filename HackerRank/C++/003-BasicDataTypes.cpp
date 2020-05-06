2d7bacc38e4b241d299eed5c465612f22ed2b002
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2020-05-06 06:09:51 -0400

    Create 003-BasicDataTypes.cpp

---
+++ HackerRank/C++/003-BasicDataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int A;
+    long int B;
+    char C;
+    float D;
+    double E;
+
+    scanf("%d %ld %c %f %lf", &A,&B,&C,&D,&E);
+
+    printf("%d\n%ld\n%c\n%f\n%lf\n", A,B,C,D,E);
+
+    return 0;
+}
